0ece83a22940ec7afe1c8ba33998379db9d9603b
Jonah Simmons <jks013@protonmail.com>
2021-03-19 19:07:47 -0700

    Have working weapon base. Have destroyable enemies.

--- Player/Weapon.gd
+++ Player/Weapon.gd
@@ -1,20 +1,33 @@
 extends Node
 
+class_name Weapon
+
 export var fireTime = 0.5
 export var magSize = 5
 export var reloadTime = 1
+export var fireRange = 100
 
-var currentAmmo = magSize
+onready var raycast = $"/root/World/Player/Head/Camera/RayCast"
+onready var ammoLabel = $"/root/World/UI/Ammo"
+
+var currentAmmo = 0
 var canFire = true
 var isReloading = false
 
+func _ready():
+	raycast.cast_to = Vector3(0, 0, -fireRange)
+	currentAmmo = magSize
+
 func _process(delta):
+	ammoLabel.set_text("Hi")
+	
 	if Input.is_action_just_pressed("primary_fire") and canFire:
 		#fire da wheapon
 		if currentAmmo > 0 and not isReloading:
 			print("Yatatatatatatata")
 			canFire = false
 			currentAmmo -= 1
+			checkColision()
 			yield(get_tree().create_timer(fireTime), "timeout")
 			canFire = true
 	
@@ -25,3 +38,12 @@ func _process(delta):
 		currentAmmo = magSize
 		isReloading = false
 		print("Gatof")
+
+
+func checkColision():
+	print("yeeters")
+	if raycast.is_colliding():
+		var collider = raycast.get_collider()
+		if collider.is_in_group("Enemies"):
+			collider.queue_free()
+			print("Killed " + collider.name)
